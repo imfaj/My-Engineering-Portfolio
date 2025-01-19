16f3a2f4e567d1fa3a95137c799870ea9fbaa514
imfaj <mfaj2697@hotmail.com>
2025-01-19 02:01:53 -0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,40 @@
   var toggle = document.getElementById("menu-toggle");
   var menu = document.getElementById("menu");
   var close = document.getElementById("menu-close");
+  /*Para Slider */
+  let currentIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+  /*FIN Para Slider */
+
+  /*INICIO PARA SLIDER */
+    // Muestra la imagen en el índice actual
+  function showSlide(index) {
+    // Asegúrate de que el índice esté dentro de los límites
+    if (index >= totalSlides) {
+        currentIndex = 0; // Volver al inicio si estamos al final
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1; // Volver al final si estamos al principio
+    } else {
+        currentIndex = index;
+    }
+
+    // Cambia la posición del slider para mostrar la imagen correspondiente
+    const slider = document.querySelector('.slider');
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`; // Desplaza las imágenes horizontalmente
+  }
+
+  // Cambia la imagen cuando se hace clic en los botones
+  function changeSlide(direction) {
+    showSlide(currentIndex + direction); // Aumenta o disminuye el índice
+  }
+
+  // Inicializa el slider mostrando la primera imagen
+  document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex); // Muestra la primera imagen al cargar
+  });
+  /*FINAL Para Slider */
+
 
   toggle.addEventListener("click", function(e) {
     if (menu.classList.contains("open")) {
